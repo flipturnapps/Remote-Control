31ac9a3b5d808cf251ae0838f3bfb93868d7c624
flipturnapps <flipturnapps@gmail.com>
2015-07-02 06:50:24 -0700

    Added RCEvents

--- src/Gui.java
+++ src/Gui.java
@@ -1,16 +1,21 @@
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class Gui extends JFrame
 {
 	private Image image;
+	private ImagePanel panel;
 	public Gui()
 	{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 500);
-		JPanel panel = new ImagePanel();
+		panel = new ImagePanel();
 		this.getContentPane().add(panel);
 		this.setVisible(true);
 	}
@@ -22,8 +27,73 @@ public class Gui extends JFrame
 		this.image = image;
 		this.repaint();
 	}
+	private class Listeners implements MouseListener, KeyListener
+	{
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) 
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) 
+		{
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e)
+		{
+			if(e.getKeyCode() != e.VK_ENTER)
+				System.out.println("notenter");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e)
+		{
+			/*
+			if(e.getKeyCode() != e.VK_ENTER)
+				client.flushEventList(eventList);
+				*/
+		}
+		
+	}
 	private class ImagePanel extends JPanel
 	{
+		public ImagePanel()
+		{
+			this.setFocusable(true);
+			this.requestFocus();
+			this.addMouseListener(new Listeners());
+			this.addKeyListener(new Listeners());
+		}
 		public void paintComponent(Graphics g)
 		{
 			g.drawImage(image, 0, 0, this.getWidth(),this.getHeight(),null);
